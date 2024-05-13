3efa79c77c99a5bb5262425ca7dbe0428afa185c
Martin R <martinribotta6@gmail.com>
2024-05-12 21:11:52 -0300

    primerCommit

---
+++ FrontEnt/src/App.tsx
@@ -0,0 +1,12 @@
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/*RUTAS A DISTINTAS PAGINAS DEL FRONTEND */}
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
