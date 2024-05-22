f2db6ed44b596c839a9b6bbd29abc9dcfd2c9061
fsilva95 <franco.silva1995@gmail.com>
2024-05-22 19:28:50 -0300

    Vuelta atras de estados globales empresa

--- FrontEnt/src/componentes/dashboard/AppHeader.tsx
+++ FrontEnt/src/componentes/dashboard/AppHeader.tsx
@@ -1,18 +1,16 @@
-import { IconButton, Box, AppBar, Toolbar, Typography} from "@mui/material";
+import { IconButton, Box, AppBar, Toolbar, Typography } from "@mui/material";
 import MenuIcon from '@mui/icons-material/Menu';
 import LogoutIcon from '@mui/icons-material/Logout';
-import { useGlobalState } from '../../contexto/GlobalStateProvider';
 
-interface AppHeaderTypes{
-  setCollapse:(item:boolean)=>void;
-  setToggle:(item:boolean)=>void;
-  broken:boolean;
-  collapsed:boolean;
-  toggled:boolean;
+interface AppHeaderTypes {
+  setCollapse: (item: boolean) => void;
+  setToggle: (item: boolean) => void;
+  broken: boolean;
+  collapsed: boolean;
+  toggled: boolean;
 }
-function AppHeader({broken,setCollapse,setToggle,collapsed,toggled}:AppHeaderTypes) {
-  const { selectedEmpresaNombre, selectedSucursalNombre } = useGlobalState();
-  
+
+function AppHeader({ broken, setCollapse, setToggle, collapsed, toggled }: AppHeaderTypes) {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="sticky">
@@ -23,29 +21,26 @@ function AppHeader({broken,setCollapse,setToggle,collapsed,toggled}:AppHeaderTyp
             color='inherit'
             aria-label="menu"
             sx={{ mr: 2 }}
-            onClick={()=>{setToggle(!toggled)}}
+            onClick={() => { setToggle(!toggled) }}
           >
             <MenuIcon />
-          </IconButton>):(
+          </IconButton>) : (
             <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-            onClick={()=>{setCollapse(!collapsed)}}
-          >
-            <MenuIcon />
-          </IconButton>
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+              onClick={() => { setCollapse(!collapsed) }}
+            >
+              <MenuIcon />
+            </IconButton>
           )}
-          <Box component="img" src="/imgs/Icono.svg" sx={{width:70,margin:1}}/>
-          <Typography variant="h6" component="div" sx={{flexGrow:1}}>
+          <Box component="img" src="/imgs/Icono.svg" sx={{ width: 70, margin: 1 }} />
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             El Buen Sabor Dashboard
           </Typography>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 2, textAlign: 'center' }}>
-            {selectedEmpresaNombre} - {selectedSucursalNombre} {/* Mostrar el nombre de la empresa y la sucursal */}
-          </Typography>
-          <Box sx={{flexGrow:1}}></Box>
+          <Box sx={{ flexGrow: 1 }}></Box>
           <IconButton title="Logout" color="inherit">
             <LogoutIcon />
           </IconButton>
--- FrontEnt/src/main.tsx
+++ FrontEnt/src/main.tsx
@@ -3,12 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
-import { GlobalStateProvider } from './contexto/GlobalStateProvider.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <GlobalStateProvider>
       <App />
-    </GlobalStateProvider>
   </React.StrictMode>,
 )
