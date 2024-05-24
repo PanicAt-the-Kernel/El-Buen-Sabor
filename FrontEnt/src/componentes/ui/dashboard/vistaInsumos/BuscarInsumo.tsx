98a1c553e9d106c0c34117782905246decbcf50a
Martin R <martinribotta6@gmail.com>
2024-05-24 18:50:30 -0300

    Vista Mobile Insumos

--- FrontEnt/src/componentes/ui/dashboard/vistaInsumos/BuscarInsumo.tsx
+++ FrontEnt/src/componentes/ui/dashboard/vistaInsumos/BuscarInsumo.tsx
@@ -1,35 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { Box, FormControl, Input, Stack } from "@mui/material";
-import BotonAgregarInsumo from './BotonAgregarInsumo';
-import TablaInsumo from './TablaInsumo';
+import BotonAgregarInsumo from "./BotonAgregarInsumo";
+import GrillaInsumos from "./GrillaInsumos";
 
 function BuscarInsumo() {
-    const [nombre, setNombre] = useState('');
+  const [nombre, setNombre] = useState("");
 
-    const handleBuscar = (e: React.FormEvent) => {
-        e.preventDefault();
-    };
+  const handleBuscar = (e: React.FormEvent) => {
+    e.preventDefault();
+  };
 
-    return (
-        <Box>
-            <Stack direction="row" spacing={2} alignItems="center">
-                <form onSubmit={handleBuscar}>
-                    <FormControl fullWidth margin="normal">
-                        <Input
-                            placeholder="Buscar insumo"
-                            id="nombreInsumo"
-                            value={nombre}
-                            onChange={(e) => setNombre(e.target.value)}
-                            sx={{ width: '300px' }}
-                        />
-                    </FormControl>
-                </form>
-                <BotonAgregarInsumo />
-            </Stack>
-            <p></p>
-            <TablaInsumo busqueda={nombre} />
+  return (
+    <Box>
+      <Stack
+        direction={{ xs: "column", sm: "column", md: "row" }}
+        spacing={{
+          xs: 1,
+          sm: 1,
+          md: 2,
+        }}
+        alignItems="center"
+      >
+        <Box component="form" onSubmit={handleBuscar}>
+          <FormControl fullWidth margin="normal">
+            <Input
+              placeholder="Buscar insumo"
+              id="nombreInsumo"
+              value={nombre}
+              onChange={(e) => setNombre(e.target.value)}
+              sx={{ width: "300px" }}
+            />
+          </FormControl>
         </Box>
-    );
+        <BotonAgregarInsumo />
+      </Stack>
+      <GrillaInsumos busqueda={nombre} />
+    </Box>
+  );
 }
 
-export default BuscarInsumo
+export default BuscarInsumo;
