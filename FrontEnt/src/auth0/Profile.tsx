e193d1f4ab7b2593ecae44de80508a4479b44c1f
JenaManinoMartin <jenamanino77@gmail.com>
2024-06-18 16:46:35 -0300

    Roles en Auth0 front

--- FrontEnt/src/auth0/Profile.tsx
+++ FrontEnt/src/auth0/Profile.tsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { jwtDecode } from "jwt-decode";
 import { JwtPayload } from "jwt-decode";
- 
+
 
 // Define la interfaz para el token decodificado
 interface DecodedToken extends JwtPayload {
@@ -24,9 +24,11 @@ export const Profile = () => {
 
                 try {
                     const tokenClaims = await getIdTokenClaims();
+                    //@ts-ignore
                     const token = tokenClaims.__raw; // Obtén el token en formato string
                     const decoded = jwtDecode<DecodedToken>(token); // Decodifica el token
                     setDecodedToken(decoded); // Guarda el token decodificado en el estado
+                    console.log("Esto es el token decoded: ", decoded)
                 } catch (error) {
                     console.error("Error decoding token:", error);
                 }
@@ -40,20 +42,25 @@ export const Profile = () => {
         return <div>Cargando...</div>;
     }
 
+    const roles = decodedToken?.["https://buen-sabor.com/roles"]?.join(", ") || "";
+
     return (
         <div>
             {!isAuthenticated && <div>No estás autenticado.</div>}
-            {isAuthenticated && user && (
+            {isAuthenticated && !roles && user && (
                 <div>
                     <h2>Perfil</h2>
-                    <p>Nombre: {user.name}</p>
-                    <p>Email: {user.email}</p>
-                    {decodedToken && (
-                        <div>
-                            <h3>Claims:</h3>
-                            <pre>{JSON.stringify(decodedToken, null, 2)}</pre>
-                        </div>
-                    )}
+                    <p><b>Nombre: </b> {user.nickname}</p>
+                    <p><b>Email: </b> {user.email}</p>
+                    <p><b>Roles: </b>Sin roles asignados.</p>
+                </div>
+            )}
+            {isAuthenticated && roles && user && (
+                <div>
+                    <h2>Perfil</h2>
+                    <p><b>Nombre: </b> {user.nickname}</p>
+                    <p><b>Email: </b> {user.email}</p>
+                    <p><b>Roles: </b> {roles}</p>
                 </div>
             )}
         </div>
