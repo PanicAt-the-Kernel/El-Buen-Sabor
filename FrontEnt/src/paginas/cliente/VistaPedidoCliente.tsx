c215133daa5549bb804f975dc3c56cc4b58e2250
Martin R <martinribotta6@gmail.com>
2024-06-27 11:50:11 -0300

    Vista Pedido Cliente V2

--- FrontEnt/src/paginas/cliente/VistaPedidoCliente.tsx
+++ FrontEnt/src/paginas/cliente/VistaPedidoCliente.tsx
@@ -13,52 +13,79 @@ import FormSelectDomicilio from "../../componentes/ui/cliente/VistaPedidoCliente
 import { useContext, useState } from "react";
 import { AttachMoney } from "@mui/icons-material";
 import { CarritoContext } from "../../context/CarritoContext";
-import { localData, savePedido } from "../../servicios/vistaInicio/FuncionesAPI";
+import {
+  localData,
+  savePedido,
+} from "../../servicios/vistaInicio/FuncionesAPI";
 import Pedido from "../../entidades/Pedido";
 import Cliente from "../../entidades/Cliente";
 import Sucursal from "../../entidades/Sucursal";
+import Domicilio from "../../entidades/Domicilio";
 
 export default function VistaPedidoCliente() {
   const [open, setOpen] = useState(false);
   const [metodoEntrega, setMetodoEntrega] = useState<string>("TAKE_AWAY");
   const [metodoPago, setMetodoPago] = useState<string>("MERCADO_PAGO");
   const [domicilio, setDomicilio] = useState<number>(0);
-  const {carrito,totalPedido,totalEnvio,setTotalPedido,vaciarCarrito } = useContext(CarritoContext);
-  const [pedido, setPedido] = useState<Pedido>(new Pedido);
-  const cliente:Cliente = localData.getCliente("Cliente");
-  const sucursal:Sucursal=localData.getSucursal("sucursal");
+  const { carrito, totalPedido, totalEnvio, setTotalPedido, vaciarCarrito } =
+    useContext(CarritoContext);
+  const [pedido, setPedido] = useState<Pedido>(new Pedido());
+  const cliente: Cliente = localData.getCliente("Cliente");
+  const sucursal: Sucursal = localData.getSucursal("sucursal");
 
   const postPedido = () => {
+    var fecha = new Date().toJSON().slice(0, 10); //Dia actual
+
+    const pedidoTemplate = {
+      ...pedido,
+      horaEstimadaFinalizacion: "22:00:00.000",
+      totalCosto: 0,
+      estado: "PENDIENTE",
+      sucursal: sucursal,
+      cliente: cliente,
+      fechaPedido: fecha,
+      factura: null,
+      detallePedidos: carrito,
+    };
+    setPedido(pedidoTemplate);
+
     //POST SIN DOMICILIO
     if (metodoEntrega == "TAKE_AWAY") {
       if (metodoPago == "EFECTIVO") {
-        alert('LLAMADA A POST CON EFECTIVO Y TAKE AWAY');
-        var fecha = new Date().toJSON().slice(0, 10); //Dia actual
-
-        const updatedPedido = {
-          ...pedido,
-          horaEstimadaFinalizacion: "22:00:00.000",
+        alert("LLAMADA A POST CON EFECTIVO Y TAKE AWAY");
+        setPedido({
+          ...pedidoTemplate,
           total: totalPedido,
-          totalCosto: 0,
-          estado: "PENDIENTE",
           tipoEnvio: metodoEntrega,
           formaPago: metodoPago,
-          sucursal:sucursal,
-          cliente:cliente,
-          fechaPedido: fecha,
-          domicilio:sucursal.domicilio,
-          factura: null,
-          detallePedidos: carrito,
-        };
-        setPedido(updatedPedido);
-        savePedido(updatedPedido,setTotalPedido,vaciarCarrito,0);
+          domicilio: sucursal.domicilio,
+        })
+        savePedido(pedidoTemplate, setTotalPedido, vaciarCarrito, 0);
       } else {
         //HACER POST Y LLAMAR MERCADOPAGO
+        setPedido({
+          ...pedidoTemplate,
+          total: totalPedido,
+          tipoEnvio: metodoEntrega,
+          formaPago: metodoPago,
+          domicilio: sucursal.domicilio,
+        })
+        savePedido(pedidoTemplate,setTotalPedido,vaciarCarrito,0);
+        //llamarMercadoPago(pedidoID)
       }
     } else {
       //POST CON DOMICILIO
+      const domi=cliente.domicilios.find((item:Domicilio)=>item.id==domicilio);
+      setPedido({
+        ...pedidoTemplate,
+        total: totalPedido,
+        tipoEnvio: metodoEntrega,
+        formaPago: metodoPago,
+        domicilio: domi!,
+      })
       //HACER POST
-      
+      savePedido(pedidoTemplate,setTotalPedido,vaciarCarrito,20);
+      //llamarMercadoPago(pedidoID)
     }
   };
 
@@ -69,7 +96,9 @@ export default function VistaPedidoCliente() {
           variant="contained"
           color="info"
           sx={{ marginBottom: 2 }}
-          onClick={() => {postPedido()}}
+          onClick={() => {
+            postPedido();
+          }}
         >
           Generar Pedido
         </Button>
@@ -80,7 +109,9 @@ export default function VistaPedidoCliente() {
           variant="contained"
           color="warning"
           sx={{ marginBottom: 2 }}
-          onClick={() => {postPedido()}}
+          onClick={() => {
+            postPedido();
+          }}
         >
           Ir a pagar
         </Button>
--- FrontEnt/src/utils/mercadoPago/CheckOutMP.tsx
+++ FrontEnt/src/utils/mercadoPago/CheckOutMP.tsx
@@ -2,14 +2,18 @@ import { Wallet, initMercadoPago } from "@mercadopago/sdk-react"
 import { Box } from "@mui/material"
 import { useEffect } from "react"
 
-export default function CheckOutMP(){
+interface CheckOutMPTypes{
+    preferenceID:string;
+}
+
+export default function CheckOutMP({preferenceID}:CheckOutMPTypes){
     useEffect(()=>{
         initMercadoPago("APP_USR-dde2f690-4c16-44fb-b305-da3c9e4c1593",{locale:"es-AR"})
     },[])
     
     return(
         <Box component="div">
-            <Wallet initialization={{preferenceId:""}} />
+            <Wallet initialization={{preferenceId:preferenceID}} />
         </Box>
     )
 }
