4cd50f3f233c8b05bddad57752e6a835648f92a4
Martin R <martinribotta6@gmail.com>
2024-07-17 16:59:08 -0300

    Fix Insumos

--- FrontEnt/src/componentes/dashboard/AppHeader.tsx
+++ FrontEnt/src/componentes/dashboard/AppHeader.tsx
@@ -1,5 +1,12 @@
-import { IconButton, Box, AppBar, Toolbar, Typography } from "@mui/material";
-import MenuIcon from '@mui/icons-material/Menu';
+import {
+  IconButton,
+  Box,
+  AppBar,
+  Toolbar,
+  Typography,
+  Stack,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
 import { useAuth0 } from "@auth0/auth0-react";
 import { Link } from "react-router-dom";
 import { Logout } from "@mui/icons-material";
@@ -10,14 +17,17 @@ export const LogOutButton = () => {
   const { logout } = useAuth0();
 
   return (
-      <Link to="#" className="btn btn-outline-light" onClick={()  =>  {
+    <Link
+      to="#"
+      className="btn btn-outline-light"
+      onClick={() => {
         logout({ logoutParams: { returnTo: window.location.origin } });
         localSession.removeRol("userRoles");
       }}
-        >
-          Cerrar sesión&nbsp;
-          <Logout />
-      </Link>
+    >
+      Cerrar sesión&nbsp;
+      <Logout />
+    </Link>
   );
 };
 
@@ -29,36 +39,58 @@ interface AppHeaderTypes {
   toggled: boolean;
 }
 
-function AppHeader({ broken, setCollapse, setToggle, collapsed, toggled }: AppHeaderTypes) {
+function AppHeader({
+  broken,
+  setCollapse,
+  setToggle,
+  collapsed,
+  toggled,
+}: AppHeaderTypes) {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="sticky">
         <Toolbar>
-          {broken ? (<IconButton
-            size="large"
-            edge="start"
-            color='inherit'
-            aria-label="menu"
-            sx={{ mr: 2 }}
-            onClick={() => { setToggle(!toggled) }}
-          >
-            <MenuIcon />
-          </IconButton>) : (
+          {broken ? (
             <IconButton
               size="large"
               edge="start"
               color="inherit"
               aria-label="menu"
               sx={{ mr: 2 }}
-              onClick={() => { setCollapse(!collapsed) }}
+              onClick={() => {
+                setToggle(!toggled);
+              }}
+            >
+              <MenuIcon />
+            </IconButton>
+          ) : (
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+              onClick={() => {
+                setCollapse(!collapsed);
+              }}
             >
               <MenuIcon />
             </IconButton>
           )}
-          <Box component="img" src="/imgs/Icono.svg" sx={{ width: 70, margin: 1 }} />
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            El Buen Sabor Dashboard
-          </Typography>
+          <Box
+            component="img"
+            src="/imgs/Icono.svg"
+            sx={{ width: 70, margin: 1 }}
+          />
+          <Stack>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              El Buen Sabor Dashboard
+            </Typography>
+            <Typography variant="body2">
+              {localSession.getSucursal("sucursal").nombre}
+            </Typography>
+          </Stack>
+
           <Box sx={{ flexGrow: 1 }}></Box>
           <LogOutButton />
         </Toolbar>
